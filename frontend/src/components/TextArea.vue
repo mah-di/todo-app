3b82af5b398cc14b56927e3fdec21b1d44ce1fd8
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2025-11-19 01:53:58 +0600

    functionalities added for managing to-do list

---
+++ frontend/src/components/TextArea.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+    'modelValue': String,
+    'name': String,
+    'label': {
+        type: String,
+        default: null
+    },
+    'placeholder': String,
+    'required': {
+        type: Boolean,
+        default: false
+    },
+    'rows': {
+        type: Number,
+        default: 4
+    }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <div class="mb-4">
+        <label v-if="label" :for="name" class="block text-gray-700 font-bold mb-2">{{ label }}</label>
+        <textarea :value="modelValue" :id="name" :name="name" class="border border-violet-200 focus:outline focus:outline-violet-300 rounded w-full py-2 px-3" :rows="rows"
+            :placeholder="placeholder" :required="required"
+            @change="$emit('update:modelValue', $event.target.value)"></textarea>
+    </div>
+</template>
